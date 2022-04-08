e7c124d261de8de9582c614129bbd7fadf3294c3
desmond <desmond@desmonds-MacBook-Pro.local>
2022-04-08 14:16:24 +0300

    task finished

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+
+  function saturdayFun(activity="roller-skate"){
+      return`This Saturday, I want to ${activity}!`
+  }
+  
+  
+  let mondayWork = function(activity="go to the office"){
+      return `This monday, i will ${activity}.`
+  }
+  
+  
+  
+  let wrapAdjective = function(desmond="*"){
+      return function(adjective="i want to bathe my dog"){
+          return `this saturday ${desmond}${adjective}${desmond}!`
+      }
+  }
+  
+  
+  
+  
